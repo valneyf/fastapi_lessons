e8c028f530becbec6b0aa6c7e1814b0306180342
Valney Filho <valney.costa.ced@prof.ce.gov.br>
2023-01-05 10:33:13 -0300

    Add db file and initial crud

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -8,6 +9,35 @@ app = FastAPI()
 models.Base.metadata.create_all(engine)
 
 
-@app.post('/blog')
-def create(request: schemas.Blog):
-    return request
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+
+    return new_blog
+
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+
+    return blogs
+
+
+@app.get('/blog/{id}')
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    return blog
