a2e3bf6422df01465a7bd6196bcb7aa5693e8315
Valney Filho <valneyf.dev@gmail.com>
2023-01-05 00:36:09 -0300

    App blog added

---
+++ blog/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from . import schemas, models
+from .database import engine
+
+app = FastAPI()
+
+
+models.Base.metadata.create_all(engine)
+
+
+@app.post('/blog')
+def create(request: schemas.Blog):
+    return request
